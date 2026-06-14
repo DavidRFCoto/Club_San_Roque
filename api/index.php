1a1d2ef31670c17396cbdd6acba96fb929b8c00e
DavidRFCoto <davidrobertoferrcoto@gmail.com>
2026-06-14 14:33:00 -0600

    fix: serve static assets with readfile, enable debug for login error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    $ext = pathinfo($uri, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'css' => 'text/css',
+        'js'  => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'svg' => 'image/svg+xml',
+        'ico' => 'image/x-icon',
+        'woff2' => 'font/woff2',
+    ];
+    header('Content-Type: ' . ($mimeTypes[$ext] ?? 'application/octet-stream'));
+    readfile(__DIR__ . '/../public' . $uri);
+    exit;
+}
+
 require __DIR__ . '/../public/index.php';
