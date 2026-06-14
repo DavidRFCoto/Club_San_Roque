4a8ecbf79439a0964cd5d772887b79d69519fb7b
DavidRFCoto <davidrobertoferrcoto@gmail.com>
2026-06-14 14:39:05 -0600

    fix: forward Vercel env vars to putenv() so Laravel can read them

--- api/index.php
+++ api/index.php
@@ -1,5 +1,11 @@
 <?php
 
+foreach ($_SERVER as $key => $value) {
+    if (is_string($value) && !getenv($key)) {
+        putenv("$key=$value");
+    }
+}
+
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
